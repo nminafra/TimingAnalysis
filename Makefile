59eba1b21c47571aca87437b6a58726b1d47a4db
Nicola Minafra <nicola.minafra@cern.ch>
2018-10-19 16:30:33 +0200

    Stripped boost dependency

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 ROOT=`root-config --cflags --libs`
 
-LIB_DIRECTORY=Oscilloscope_analyzeData
-LIB_NAME=Oscilloscope_analyzeData
+LIB_DIRECTORY=TimingAnalysis
+LIB_NAME=TimingAnalysis
 CHECKFORUPDATE=$(LIB_DIRECTORY)/$(LIB_NAME).a $(LIB_DIRECTORY)/include/$(LIB_NAME).h $(LIB_DIRECTORY)/include/timingAlgorithm.h
 
 progs=$(LIB_DIRECTORY)/$(LIB_NAME).a example_analyzeData
@@ -11,9 +11,8 @@ all:$(progs)
 $(LIB_DIRECTORY)/$(LIB_NAME).a: $(objects:%=$(LIB_DIRECTORY)/src/%) $(LIB_DIRECTORY)/include/timingAlgorithm.h
 	$(MAKE) -C $(LIB_DIRECTORY)
 
-example_analyzeData:%:%.cxx $(CHECKFORUPDATE) 
-	g++ -o $@ $< -I$(LIB_DIRECTORY)/include  $(LIB_DIRECTORY)/$(LIB_NAME).a $(ROOT) -z muldefs -O3 -std=c++11 -lboost_program_options
-	
+example_analyzeData:%:%.cxx $(CHECKFORUPDATE)
+	g++ -o $@ $< -I$(LIB_DIRECTORY)/include  $(LIB_DIRECTORY)/$(LIB_NAME).a $(ROOT) -z muldefs -O3 -std=c++11
+
 clean:
 	rm -r example_analyzeData $(LIB_DIRECTORY)/*.a $(LIB_DIRECTORY)/src/*.o
-
--- example_analyzeData.cxx
+++ example_analyzeData.cxx
@@ -1,68 +1,96 @@
-#include <Oscilloscope_analyzeData.h>
+#include <TimingAnalysis.h>
 #include <timingAlgorithm.h>
 
 #include <TFileCollection.h>
-#include <boost/lexical_cast.hpp>
-#include "boost/program_options.hpp"
-
-namespace po = boost::program_options;
 
 int main (int argc, char** argv)
 {
-  // Declare the supported options.
-  po::options_description desc("Allowed options");
-  desc.add_options()
-  ("help,h", "produce help message")
-  ("firstchannel,f", po::value<int>()->default_value(0), "First channel to analyze")
-  ("secondchannel,s", po::value<int>()->default_value(1), "Second channel to analyze")
-  ("cfd_threshold,c", po::value<double>()->default_value(0.4), "CFD fraction")
-  ("threshold_ch1,t", po::value<double>()->default_value(-0.1), "Threshold for ch 1, negative for negative signals (V)")
-  ("threshold_ch2,w", po::value<double>()->default_value(-0.1), "Threshold for ch 2, negative for negative signals (V)")
-  ("lowpass,p", po::value<double>()->default_value(0), "Lowpass filter frequency (Hz)")
-  ("hysteresis,h", po::value<double>()->default_value(1e-3), "Hysteresis for the discriminator (V)")
-  ("treename,n", po::value<std::string>()->default_value("pulse"), "Name of the TTree")
-  ("outputdir,o", po::value<std::string>()->default_value("./Results"), "output directory")
-  ("filename,i", po::value<std::string>(), "input file");
-  
-  po::positional_options_description positionalOptions; 
-  positionalOptions.add("filename", 1); 
-  
-  po::variables_map vm; 
-  po::store(po::command_line_parser(argc, argv).options(desc).positional(positionalOptions).run(), vm);
-  
-  if ( vm.count("help") ) { 
-    std::cout << desc << std::endl; 
-    return 0; 
-  } 
-  
+  std::string filename("");
+  std::string outputdir("./Results");
+  int firstchannel=0;
+  int secondchannel=1;
+  float cfd_threshold=0.4;
+  float threshold_ch1=-0.1;
+  float threshold_ch2=-0.1;
+  float lowpass=0;
+  float hysteresis=1e-3;
+
+  for (int i=1; i<argc; ++i)
+  {
+    if (argv[i][0] == '-') {
+      // Option found
+      std::string option(argv[i]);
+      if ( option == "-h" || option == "--help" ) {
+        std::cout << "List of options: " << std::endl;
+        std::cout << "-h [ --help ]                         produce help message" << std::endl;
+        std::cout << "-f [ --firstchannel ] arg (=0)        First channel to analyze" << std::endl;
+        std::cout << "-s [ --secondchannel ] arg (=1)       Second channel to analyze" << std::endl;
+        std::cout << "-c [ --cfd_threshold ] arg (=0.4)     CFD fraction" << std::endl;
+        std::cout << "-t [ --threshold_ch1 ] arg (=-0.1)    Threshold for ch 1, negative for" << std::endl;
+        std::cout << "                                      negative signals (V)" << std::endl;
+        std::cout << "-w [ --threshold_ch2 ] arg (=-0.1)    Threshold for ch 2, negative for" << std::endl;
+        std::cout << "                                      negative signals (V)" << std::endl;
+        std::cout << "-p [ --lowpass ] arg (=0)             Lowpass filter frequency (Hz)" << std::endl;
+        std::cout << "-o [ --outputdir ] arg (=./Results)   output directory" << std::endl;
+        std::cout << "-i [ --filename ] arg                 input file" << std::endl;
+        return 0;
+      }
+      std::string value(argv[++i]);
+
+      if ( option == "-f" || option == "--firstchannel" )
+        firstchannel = std::stoi(value);
+      if ( option == "-s" || option == "--secondchannel" )
+        secondchannel = std::stoi(value);
+      if ( option == "-i" || option == "--filename" )
+        filename = value;
+      if ( option == "-c" || option == "--cfd_threshold" )
+        cfd_threshold = std::stof(value);
+      if ( option == "-t" || option == "--threshold_ch1" )
+        threshold_ch1 = std::stof(value);
+      if ( option == "-w" || option == "--threshold_ch2" )
+        threshold_ch2 = std::stof(value);
+      if ( option == "-p" || option == "--lowpass" )
+        lowpass = std::stof(value);
+      if ( option == "-o" || option == "--outputdir" )
+        outputdir = value;
+      if ( option == "-i" || option == "--filename" )
+        filename = value;
+    }
+  }
+
+  if (filename == "") {
+    std::cout << "Input file required! For help use:" << std::endl;
+    std::cout << argv[0] << " --help" << std::endl;
+    return 0;
+  }
+
   // Creating the analysis object from data TTree
-  std::string filename(vm["filename"].as<std::string>());
   TFileCollection * fc_example = new TFileCollection();
-  TChain * c_example = new TChain(vm["treename"].as<std::string>().c_str());
+  TChain * c_example = new TChain("pulse");
   fc_example->Add(filename.c_str());
   c_example->AddFileInfoList((TCollection*)fc_example->GetList());
 
-  Oscilloscope_analyzeData example_analyzeData(c_example);
+  TimingAnalysis example_analyzeData(c_example);
 
   // Output file
   TString filenameTail("_result_");
-  filenameTail+=vm["firstchannel"].as<int>();
+  filenameTail+=firstchannel;
   filenameTail+="_";
-  filenameTail+=vm["secondchannel"].as<int>();
+  filenameTail+=secondchannel;
   filename.insert(filename.size()-5,filenameTail.Data());
   filename.erase(0,filename.find_last_of('/',filename.size()));
-  filename.insert(0,vm["outputdir"].as<std::string>());
+  filename.insert(0,outputdir);
   std::cout<<filename<<std::endl;
-  TFile * f_root = new TFile (filename.c_str(),"RECREATE");  
-    
-  std::cout<<"Filling "<<filename<<" with "<<vm["filename"].as<std::string>()<<std::endl;    
-   
+  TFile * f_root = new TFile (filename.c_str(),"RECREATE");
 
-  AlgorithmParameters par(vm["cfd_threshold"].as<double>(),vm["threshold_ch1"].as<double>(),vm["threshold_ch2"].as<double>(),vm["lowpass"].as<double>(),vm["hysteresis"].as<double>(),-0.3,0.3,-0.4,0.4);
+  std::cout<<"Filling "<<filename<<" with "<<filename<<std::endl;
 
-   double timeres_ps = example_analyzeData.executeTimeDifference<AlgorithmParameters>(f_root, ComputeExactTimeCFD, par, vm["firstchannel"].as<int>(), vm["secondchannel"].as<int>())*1e12;
+//AlgorithmParameters(const double cfdRatio, const double threshold_ch0=.0, const double threshold_ch1=.0, const double sigma=0, const double hysteresis=1e-3, const double minCh0=-10., const double maxCh0=10., const double minCh1=-10., const double maxCh1=10., const double baseline_n=0.15)
+  AlgorithmParameters par(cfd_threshold,threshold_ch1,threshold_ch2,lowpass,hysteresis,-0.3,0.3,-0.4,0.4,0.1);
+
+   double timeres_ps = example_analyzeData.executeTimeDifference<AlgorithmParameters>(f_root, ComputeExactTimeCFD, par, firstchannel, secondchannel)*1e12;
    std::cout << "\t\t\tTime difference: " << timeres_ps << " ps" << std::endl;
-  
+
   f_root->Close();
   return 0;
 }
